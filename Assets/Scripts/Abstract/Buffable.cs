61cc5b96ad74c5cefc0f67e2f5ff55f078108190
Xeronel <ripster1121@gmail.com>
2014-05-28 18:23:20 -0400

    Created new Buffable type

---
+++ Assets/Scripts/Abstract/Buffable.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Buffable : MonoBehaviour
+{
+    public abstract float Speed { get; set; }
+    public abstract Sprite StartSprite { get; set; }
+    public abstract Vector2 Destination { get; set; }
+}
