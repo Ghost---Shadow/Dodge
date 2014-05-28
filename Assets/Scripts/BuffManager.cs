e91bd1141ef84cda93382e32cae27fefa48e12ba
TReische <treische@wesco.com>
2014-05-28 14:39:16 -0400

    Created buff manager

---
+++ Assets/Scripts/BuffManager.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(Enemy))]
+public class BuffManager : MonoBehaviour
+{
+    public float BuffDuration = 3;
+
+    Component[] buffList;
+    SpriteRenderer spriteRenderer;
+    Sprite originalSprite;
+    Enemy enemy;
+    float originalSpeed;
+    float originalWanderRadius;
+    float nextBuffTime;
+
+    // Use this for initialization
+    void Start()
+    {
+        buffList = GetComponents(typeof(Buff));
+
+        enemy = (Enemy)GetComponent(typeof(Enemy));
+        originalSpeed = enemy.Speed;
+        originalWanderRadius = enemy.WanderRadius;
+
+        spriteRenderer = (SpriteRenderer)GetComponent(typeof(SpriteRenderer));
+        originalSprite = spriteRenderer.sprite;
+    }
+
+    void Update()
+    {
+        if (Time.time >= nextBuffTime)
+        {
+            int rand = Random.Range(0, buffList.Length + 5);
+            Buff b;
+
+            RemoveBuff();
+
+            if (rand < buffList.Length)
+            {
+                b = (Buff)buffList[rand];
+                b.ApplyBuff();
+            }
+
+            nextBuffTime += BuffDuration;
+        }
+    }
+
+    void RemoveBuff()
+    {
+        enemy.Speed = originalSpeed;
+        enemy.WanderRadius = originalWanderRadius;
+        spriteRenderer.sprite = originalSprite;
+    }
+}
