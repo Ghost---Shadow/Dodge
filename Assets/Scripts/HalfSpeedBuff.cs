f3ca051667753ae850b0d2ba264fd5907f8045c2
TReische <treische@wesco.com>
2014-05-28 14:40:09 -0400

    Created HalfSpeed buff

---
+++ Assets/Scripts/HalfSpeedBuff.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HalfSpeedBuff : Buff
+{
+    public Sprite buffSprite;
+
+    SpriteRenderer spriteRenderer;
+    Enemy enemy;
+
+    // Use this for initialization
+    void Start()
+    {
+        enemy = (Enemy)gameObject.GetComponent(typeof(Enemy));
+        spriteRenderer = (SpriteRenderer)gameObject.GetComponent(typeof(SpriteRenderer));
+    }
+
+    public override void ApplyBuff()
+    {
+        enemy.Speed *= 0.5f;
+        spriteRenderer.sprite = buffSprite;
+    }
+
+    public override string BuffName
+    {
+        get { return "Half Speed"; }
+    }
+}
