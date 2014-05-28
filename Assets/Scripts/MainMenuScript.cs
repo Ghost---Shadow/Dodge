18ffb1facc2ddbe6d221c25b9fe3706fd4c14415
Xeronel <ripster1121@gmail.com>
2014-05-27 21:13:55 -0400

    Created menu buttons

--- Assets/Scripts/MainMenuScript.cs
+++ Assets/Scripts/MainMenuScript.cs
@@ -4,6 +4,7 @@ using System.Collections;
 [RequireComponent(typeof(Texture))]
 public class MainMenuScript : MonoBehaviour
 {
+    public Texture BannerTexture;
     public Texture PlayTexture;
     public Texture OptionsTexture;
     public Texture QuitTexture;
@@ -17,6 +18,10 @@ public class MainMenuScript : MonoBehaviour
             Debug.LogError("Button texture is missing!");
 
         currentGUIMethod = MainMenu;
+
+        //banner.transform.position = Vector3.zero;
+        //banner.transform.localScale = Vector3.zero;
+        //banner.pixelInset = new Rect(50, 50, 100, 100);
     }
 
     void MainMenu()
@@ -24,13 +29,38 @@ public class MainMenuScript : MonoBehaviour
         int width = Screen.width;
         int height = Screen.height;
 
-        if (GUI.Button(new Rect(width / 3, height / 6, 200, 100), PlayTexture))
+        // Draw banner
+        GUI.DrawTexture(new Rect(width * .1f, height * .1f, 300, 100), BannerTexture);
+
+        // Create buttons
+        bool PlayButton = GUI.Button(new Rect(width * .1f, height / 3, width * .105f, height * .092f), PlayTexture);
+        bool OptionsButton = GUI.Button(new Rect(width * .65f, height * .5f, width * .2f, height * .092f), OptionsTexture);
+        bool QuitButton = GUI.Button(new Rect(width, height, 200, 100), QuitTexture);
+
+        // Handle button presses
+        if (PlayButton)
             currentGUIMethod = Play;
+
+        if (OptionsButton)
+            currentGUIMethod = Options;
+
+        if (QuitButton)
+            currentGUIMethod = Quit;
     }
 
     void Play()
     {
         Application.LoadLevel(1);
+    }
+
+    void Options()
+    {
+
+    }
+
+    void Quit()
+    {
+        Application.Quit();
     }
 
     public void OnGUI()
