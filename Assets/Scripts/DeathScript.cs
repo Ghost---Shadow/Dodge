9b173af614272528144b45104b75babc0e0abf53
Xeronel <ripster1121@gmail.com>
2014-05-29 00:56:08 -0400

    Removed unused methods

--- Assets/Scripts/DeathScript.cs
+++ Assets/Scripts/DeathScript.cs
@@ -5,18 +5,6 @@ public class DeathScript : MonoBehaviour
 {
     static int DeathCount;
 
-    // Use this for initialization
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     public static void IncrementDeath()
     {
         DeathCount++;
